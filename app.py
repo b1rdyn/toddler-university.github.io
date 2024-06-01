ce9c302eade4203c653c2b45f547d0232955d6c2
b1rdyn <lumi.soukko@gmail.com>
2024-06-01 12:09:27 +0300

    kirjainpeli

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import random
+import string
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/sanapeli')
@@ -14,9 +16,22 @@ def sanapeli():
 def numeropeli():
     return render_template('numeropeli.html')
 
-@app.route('/kirjainpeli')
+@app.route('/kirjainpeli', methods=['GET', 'POST'])
 def kirjainpeli():
-    return render_template('kirjainpeli.html')
+    message = ''  # Alustetaan muuttuja tyhjällä merkkijonolla
+    if request.method == 'GET':
+        letter_to_display = random.choice(string.ascii_uppercase)
+    else:
+        user_input = request.form['input_letter'].upper()
+        correct = (user_input == request.form['hidden_letter'])
+        if correct:
+            message = 'Oikein!'
+            letter_to_display = random.choice(string.ascii_uppercase)  # Uusi kirjain oikean vastauksen jälkeen
+        else:
+            message = 'Yritä uudelleen'
+            letter_to_display = request.form['hidden_letter']
+    return render_template('kirjainpeli.html', letter=letter_to_display, message=message)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
