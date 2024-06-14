9e1549dba31883bca63bf694be381ad50b845a96
b1rdyn <lumi.soukko@gmail.com>
2024-06-14 16:53:54 +0300

    sanapeli
    sanapeli has been created and fixed

--- sanapeli.py
+++ sanapeli.py
@@ -1,7 +1,27 @@
 from flask import Blueprint, render_template
+from flask import Flask, render_template, url_for
+import random
+import os
 
 sanapeli_bp = Blueprint('sanapeli', __name__, template_folder='templates')
 
+app = Flask(__name__)
+
+PICTURE_FILE = 'static/sanapeli/'
+
 @sanapeli_bp.route('/sanapeli')
 def sanapeli():
-    return render_template('sanapeli.html')
+    picture_files = os.listdir(PICTURE_FILE)
+    random_image = random.choice(picture_files)
+    word = os.path.splitext(random_image)[0]
+    image_route = url_for('static', filename=f'sanapeli/{random_image}')
+    return render_template('sanapeli.html', kuva_url=image_route, sana=word)
+
+
+@sanapeli_bp.route('/sanapeli/next')
+def sanapeli_next():
+    return sanapeli()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
