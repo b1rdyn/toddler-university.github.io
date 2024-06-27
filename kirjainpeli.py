f07c4b484ae8c589d3ca9b9e35a5a150b89e7e25
b1rdyn <lumi.soukko@gmail.com>
2024-06-27 23:26:01 +0300

    game instructions added

--- kirjainpeli.py
+++ kirjainpeli.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Flask
 from flask import request
 import random
 import string
 
 kirjainpeli_bp = Blueprint('kirjainpeli', __name__, template_folder='templates')
+
+app = Flask(__name__)
 
 @kirjainpeli_bp.route('/kirjainpeli', methods=['GET', 'POST'])
 def kirjainpeli():
@@ -20,3 +22,8 @@ def kirjainpeli():
             message = 'Yritä uudelleen'
             letter_to_display = request.form['hidden_letter']
     return render_template('kirjainpeli.html', letter=letter_to_display, message=message)
+
+
+if __name__ == '__main__':
+    app.register_blueprint(kirjainpeli_bp, url_prefix='/kirjainpeli')
+    app.run(debug=True)
